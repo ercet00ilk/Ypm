ed4a527f86c88f9479eb768a219e287a8f4809f7
Erdal ÇETİN <erdalcetin@windowslive.com>
2018-04-16 14:57:30 +0300

    v1@123

--- YPM.SuretVarlik/Mulk/Suret/Urun/Kategori/UrunOzellikSuret.cs
+++ YPM.SuretVarlik/Mulk/Suret/Urun/Kategori/UrunOzellikSuret.cs
@@ -1,13 +1,24 @@
-﻿namespace YPM.SuretVarlik.Mulk.Suret.Urun.Kategori
+﻿using System.ComponentModel;
+
+namespace YPM.SuretVarlik.Mulk.Suret.Urun.Kategori
 {
     public class UrunOzellikSuret
     {
         public int UrunOzellikId { get; set; }
+
+        [DisplayName("Adı")]
         public string Ad { get; set; }
+
+        [DisplayName("Kategori Sayısı")]
         public int KategoriSayisi { get; set; }
+
+        [DisplayName("Özellik Sayısı")]
         public int OzellikSayisi { get; set; }
+
+        [DisplayName("Durum")]
         public bool Durum { get; set; }
 
+        [DisplayName("Seçim")]
         public bool SeciliMi { get; set; }
     }
 }
